1c9e293979dceaa9d3d4cb92f2ba9e7f3f8f9eba
[charitha] <[charitha@agrithmics.lk]>
2024-11-19 09:14:41 +0530

    change category page

--- src/categories/categoryPage.js
+++ src/categories/categoryPage.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLanguage } from '../language/LanguageContext';
 import categories from './categories';
+import Services from './Services';
 
 function CategoryPage() {
   const navigate = useNavigate();
   const { language } = useLanguage(); // Extract language using the custom hook
 
+  useEffect(() => {
+    getAllCategories();
+  });
+
   const handleCategoryClick = (categoryId) => {
     navigate(`/products/${categoryId}`);
   };
+
+  async function getAllCategories() {
+    const categories = await Services.getAllCategories();
+
+  }
 
   return (
     <div className="category-page">
