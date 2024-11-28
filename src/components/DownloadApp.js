9a974c9f4c50b2093cd2a4dc4eb9b9750b8bfba1
JKoriginal <92023041+JKoriginal@users.noreply.github.com>
2024-11-28 15:59:26 +0530

    Data fetches successfully from the db to the Productdetails Page

---
+++ src/components/DownloadApp.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+const DownloadApp = ({ qrCodes }) => {
+    return (
+        <div className="product-qr-codes">
+            <h2>Download the App</h2>
+            <div className="qr-images">
+                <img src={qrCodes.appStore} alt="App Store QR" />
+                <img src={qrCodes.playStore} alt="Play Store QR" />
+                <img src={qrCodes.huaweiStore} alt="Huawei Store QR" />
+            </div>
+        </div>
+    );
+};
+
+export default DownloadApp;
